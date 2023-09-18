a584b81653906cc2b32ebe4fc9284d6c203c7eab
atmmtawa <134715890+atmmtawa@users.noreply.github.com>
2023-09-18 15:59:18 +0300

    allowed hosts

--- Iris/settings.py
+++ Iris/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zmvvfn#+c=xg!r$l%yxuua%mqsgy(2@db0=b1s-2evg%iy%-a@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
