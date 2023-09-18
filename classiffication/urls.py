6c2e0795b58e3058a315b410b4423ad13c4bb697
atmmtawa <134715890+atmmtawa@users.noreply.github.com>
2023-09-18 16:21:36 +0300

    added csv upload

--- classiffication/urls.py
+++ classiffication/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
 
     path('', views.classiffier, name ="classiffier"),
+    path('', views.predict_view, name ="predict_view"),
 ]
--- classiffication/views.py
+++ classiffication/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+import pandas as pd
+from django.http import HttpResponse
+from django.shortcuts import render
+from . import your_machine_learning_model
+
+def predict_view(request):
+    if request.method == 'POST':
+        csv_file = request.FILES['csv_file']
+        df = pd.read_csv(csv_file)
+        
+        # Make predictions using your machine learning model
+        predictions = your_machine_learning_model.predict(df)
+
+        # Add predictions to the DataFrame
+        df['predicted_target'] = predictions
+
+        # Convert the DataFrame to CSV
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="predictions.csv"'
+        df.to_csv(response, index=False)
+
+        return response
+    else:
+        return render(request, 'upload.html')
 
 # Create your views here.
 def classiffier(request):
