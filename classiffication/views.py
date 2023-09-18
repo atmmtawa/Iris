498f959a97f8d3dfcafa1eb73023732357cc2672
atmmtawa <134715890+atmmtawa@users.noreply.github.com>
2023-09-18 16:49:12 +0300

    backback

--- classiffication/views.py
+++ classiffication/views.py
@@ -2,27 +2,27 @@ from django.shortcuts import render
 import pandas as pd
 from django.http import HttpResponse
 from django.shortcuts import render
-from . import ml_model
+# from . import ml_model
 
-def predict_view(request):
-    if request.method == 'POST':
-        csv_file = request.FILES['csv_file']
-        df = pd.read_csv(csv_file)
+# def predict_view(request):
+#     if request.method == 'POST':
+#         csv_file = request.FILES['csv_file']
+#         df = pd.read_csv(csv_file)
         
-        # Make predictions using your machine learning model
-        predictions = ml_model.predict(df)
+#         # Make predictions using your machine learning model
+#         predictions = ml_model.predict(df)
 
-        # Add predictions to the DataFrame
-        df['predicted_target'] = predictions
+#         # Add predictions to the DataFrame
+#         df['predicted_target'] = predictions
 
-        # Convert the DataFrame to CSV
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="predictions.csv"'
-        df.to_csv(response, index=False)
+#         # Convert the DataFrame to CSV
+#         response = HttpResponse(content_type='text/csv')
+#         response['Content-Disposition'] = 'attachment; filename="predictions.csv"'
+#         df.to_csv(response, index=False)
 
-        return response
-    else:
-        return render(request, 'upload.html')
+#         return response
+#     else:
+#         return render(request, 'upload.html')
 
 # Create your views here.
 def classiffier(request):
