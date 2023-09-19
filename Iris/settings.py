71783c05e25ea1e82a76b04646a67534a7b0fd1b
atmmtawa <134715890+atmmtawa@users.noreply.github.com>
2023-09-19 14:52:37 +0300

    Update settings.py

--- Iris/settings.py
+++ Iris/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -119,9 +120,22 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+# if 'DJANGO_SETTINGS_MODULE' in os.environ:
+#     # Production settings
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'))
+# else:
+    # Development settings
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+    
+
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
